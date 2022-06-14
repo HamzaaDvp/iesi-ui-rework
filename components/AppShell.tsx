77909b22820e57dec5fe71a6d871099d0e1bdb91
HamzaaDvp <hamzaa.dvp@gmail.com>
2022-06-14 15:07:02 +0200

    implement login design page

---
+++ components/AppShell.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import {AppBar, Box, IconButton, Toolbar, Typography} from "@mui/material";
+import MenuIcon from '@mui/icons-material/Menu';
+
+type TProps = {
+    children: React.ReactNode
+}
+
+export default function Appshell({ children }: TProps) {
+    return (
+        <>
+            <Box sx={{ flexGrow: 1, mb: 8}}>
+                <AppBar position="static">
+                    <Toolbar>
+                        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                            IESI
+                        </Typography>
+                        <IconButton
+                            size="large"
+                            edge="end"
+                            color="inherit"
+                            aria-label="menu"
+                            sx={{ mr: 2 }}
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                    </Toolbar>
+                </AppBar>
+            </Box>
+            {children}
+        </>
+    )
+}
